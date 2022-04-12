f44b40db9f405e016b9a9a8d2fe5cdd694aad12b
Thomas Kreuzer <thomas.kreuzer@uni-wuerzburg.de>
2022-04-12 16:25:41 +0200

    As Darkness Falls

---
+++ src/laslnnet/__init__.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+from torch import Tensor
+
+from .chirp import chirp
+from . import pyst
+from . import xyston as xy
+
+
+class Dost2:
+    def __init__(self, im):
+        self._N = len(im)
+        self._n = 2 * int(np.log2(self._N)) - 1
+        self._b = pyst.dst2(im)
+
+    def __array__(self, dtype=complex):
+        a = np.zeros((self._N, self._N, self._n, self._n), dtype=dtype)
+        for x in range(self._N):
+            for y in range(self._N):
+                a[x, y, :, :] = pyst.freqdomain(self._b, x, y)
+        return a
+
+    def __iter__(self):
+        for x in range(self._N):
+            for y in range(self._N):
+                yield pyst.freqdomain(self._b, x, y)
+
+    def totensor(self):
+        X = np.array([[self.__array__()]])
+        X = np.stack((X.real, X.imag), axis=1)
+        return torch.Tensor(X)
+
+
+class LASLNNet(nn.Module):
+    def __init__(self, dim: int) -> None:
+        super(LASLNNet, self).__init__()
+        self.conv = nn.Sequential(
+            xy.CConv4d(1, 2, kernel_size=2),
+            nn.ReLU(inplace=True),
+            xy.CConv4d(2, 4, kernel_size=2),
+            nn.ReLU(inplace=True),
+            xy.CConv4d(4, 8, kernel_size=2),
+            nn.ReLU(inplace=True),
+            xy.CConv4d(8, 16, kernel_size=2),
+            nn.ReLU(inplace=True),
+            xy.CConv4d(16, 32, kernel_size=2),
+            nn.ReLU(inplace=True),
+            xy.CConv4d(32, 64, kernel_size=2),
+            nn.ReLU(inplace=True),
+        )
+        n = 2 * int(np.log2(dim)) - 7
+        N = 64 * (dim - 6) ** 2 * n**2
+        self.fc = nn.Linear(in_features=N, out_features=8)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.conv(x)
+        x = x.flatten(start_dim=2, end_dim=-1)
+        print(x.shape)
+        x = self.fc(x)
+        return x
+
+
+def test_cnn():
+    h = chirp()
+    S = Dost2(h)
+    x = S.totensor()
+    print(x.shape)
+    lnet = LASLNNet(h.shape[0])
+    y = lnet.forward(x)
+    print(y.shape)
---
+++ src/laslnnet/xyston.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch import Tensor
+from torch.nn.common_types import _size_4_t
+from torch.nn.modules.utils import _quadruple
+
+from typing import Optional, Union
+
+
+class Conv4d(nn.modules.conv._ConvNd):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: _size_4_t,
+        stride: _size_4_t = 1,
+        padding: Union[str, _size_4_t] = 0,
+        dilation: _size_4_t = 1,
+        groups: int = 1,
+        bias: bool = True,
+        device=None,
+        dtype=None,
+    ) -> None:
+        factory_kwargs = {"device": device, "dtype": dtype}
+        kernel_size_ = _quadruple(kernel_size)
+        stride_ = _quadruple(stride)
+        padding_ = padding if isinstance(padding, str) else _quadruple(padding)
+        dilation_ = _quadruple(dilation)
+        super(Conv4d, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size_,
+            stride_,
+            padding_,
+            dilation_,
+            False,
+            _quadruple(0),
+            groups,
+            bias,
+            "zeros",
+            **factory_kwargs,
+        )
+
+    def _conv_forward(
+        self, input: Tensor, weight: Tensor, bias: Optional[Tensor]
+    ) -> Tensor:
+        b, c_i, l_i, d_i, h_i, w_i = input.shape
+        l_k = self.kernel_size[0]
+        if isinstance(self.padding, tuple):
+            pad_ = self.padding[0]
+            padding_ = self.padding[1:]
+        else:
+            padding_ = self.padding
+            pad_ = 0 if self.padding == "valid" else -1
+        l_o = (
+            l_i
+            if pad_ < 0
+            else (l_i + 2 * pad_ - self.dilation[0] * (l_k - 1) - 1) // self.stride[0]
+            + 1
+        )
+        o_f = l_o * [None]
+        for i in range(l_k):
+            for j in range(l_i):
+                n = j - (i - l_k // 2) - (l_i - l_o) // 2
+                if n < 0 or n >= l_o:
+                    continue
+                f = F.conv3d(
+                    input[:, :, j, :].view(b, c_i, d_i, h_i, w_i),
+                    weight[:, :, i, :, :],
+                    bias,
+                    self.stride[1:],
+                    padding_,
+                    self.dilation[1:],
+                    self.groups,
+                )
+                if o_f[n] is None:
+                    o_f[n] = f
+                else:
+                    o_f[n] += f
+        return torch.stack(o_f, dim=2)
+
+    def forward(self, input: Tensor) -> Tensor:
+        return self._conv_forward(input, self.weight, self.bias)
+
+
+class _CConvNd(nn.Module):
+    def __init__(self, cls, *args, **kwargs) -> None:
+        super(_CConvNd, self).__init__()
+        self.conv = cls(*args, **kwargs)
+
+    def forward(self, x: Tensor) -> Tensor:
+        m_r = self.conv(x[:, 0])
+        m_i = self.conv(x[:, 1])
+        r = m_r - m_i
+        i = m_i + m_r
+        return torch.stack((r, i), dim=1)
+
+
+def CConv3d(*args, **kwargs):
+    return _CConvNd(nn.Conv3d, *args, **kwargs)
+
+
+def CConv4d(*args, **kwargs):
+    return _CConvNd(Conv4d, *args, **kwargs)

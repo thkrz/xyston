98932b81913aa6bdd83b4b459ce9f9c3e289de64
Thomas Kreuzer <thomas.kreuzer@uni-wuerzburg.de>
2022-04-12 16:25:34 +0200

    Ares

--- src/tsig.py
+++ src/tsig.py
@@ -1,21 +1,5 @@
-import numpy as np
-import xyston as xy
-
-
-def chirp22(n=64):
-    m = n // 2
-    h = np.zeros((n, n))
-    for x in range(n):
-        f = 10.0 * np.cos(2.0 * np.pi * (0.15 * x) * x / 64.0)
-        for y in range(n):
-            xx = (x - m) ** 2
-            yy = (y - m) ** 2
-            h[x, y] = f * np.exp(-0.5 * (xx / n + yy / m))
-    return h
+from laslnnet import test_cnn
 
 
 if __name__ == "__main__":
-    im = chirp22()
-    S = xy.dost2(im)
-    a = np.array(S)
-    print(a.shape)
+    test_cnn()

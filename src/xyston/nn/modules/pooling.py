000f451a5f050740ad10707f8bb0f80a6ec9e46d
Thomas Kreuzer <thomas.kreuzer@uni-wuerzburg.de>
2022-04-29 08:44:59 +0200

    The Apple

---
+++ src/xyston/nn/modules/pooling.py
@@ -0,0 +1,39 @@
+from torch import Tensor
+from torch.nn.common_types import _size_4_t
+from torch.nn.modules.pooling import _AvgPoolNd
+from torch.nn.modules.utils import _quadruple
+
+from .. import functional as F
+
+
+class AvgPool4d(_AvgPoolNd):
+    kernel_size: _size_4_t
+    stride: _size_4_t
+    padding: _size_4_t
+    ceil_mode: bool
+    count_include_pad: bool
+
+    def __init__(
+        self,
+        kernel_size: _size_4_t,
+        stride: _size_4_t = None,
+        padding: _size_4_t = 0,
+        ceil_mode: bool = False,
+        count_include_pad: bool = True,
+    ) -> None:
+        super(AvgPool4d, self).__init__()
+        self.kernel_size = _quadruple(kernel_size)
+        self.stride = _quadruple(stride if stride is not None else kernel_size)
+        self.padding = _quadruple(padding)
+        self.ceil_mode = ceil_mode
+        self.count_include_pad = count_include_pad
+
+    def forward(self, input: Tensor) -> Tensor:
+        return F.avg_pool4d(
+            input,
+            self.kernel_size,
+            self.stride,
+            self.padding,
+            self.ceil_mode,
+            self.count_include_pad,
+        )
